70577c64549c4b8d02657aed918f8696ca6ae73a
Melekh11 <cktkutxd@gmail.com>
2023-10-18 03:25:13 +0300

    cors in bachend

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-# from models import User
 from database import Base, engine
 from routes.users import user_router
 from routes.actions import action_router
 
 
 Base.metadata.create_all(bind=engine)
-# db = SessionLocal()
-# user = User(login="init user 1", hashed_password="password123")
-# db.add(user)
-# db.commit()
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 app.include_router(user_router)
 app.include_router(action_router)
 
